927f6b4af5d99e29c22689f8e7f296cbdd13b5ec
gursidak <gursidak2000@gmail.com>
2024-01-15 23:27:06 +0530

    chore: setup redux-toolkit and redux related stuff

---
+++ state/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// config the store 
+const store= configureStore({
+   reducer: {
+   }
+})
+
+// export default the store 
+export default store
+
