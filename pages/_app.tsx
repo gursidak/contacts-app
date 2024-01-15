7827013cc10921af5287098f23880aa2b33c5366
gursidak <gursidak2000@gmail.com>
2024-01-15 23:27:47 +0530

    chore: setup material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import store from "@/state/store";
 import "@/styles/globals.css";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 
 export default function App(props: AppProps) {
   const {pageProps, Component} = props;
   return (
+    <AppCacheProvider {...props}>
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
+    </AppCacheProvider>
   );
 }
