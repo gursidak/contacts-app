942246d5d64860d7130ea950dfb83dd1bbc02327
gursidak <gursidak2000@gmail.com>
2024-01-17 16:26:22 +0530

    fixed: random function break

--- components/ui/BackgroundLetterAvatar.tsx
+++ components/ui/BackgroundLetterAvatar.tsx
@@ -1,21 +1,12 @@
 import { Avatar } from "@mui/material";
 
-function stringToColor(string: string) {
-  let hash = 0;
-  let i;
+function getRandomHexColor(): string {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
 
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
 
   return color;
 }
@@ -23,15 +14,12 @@ function stringToColor(string: string) {
 function stringAvatar(name: string) {
   return {
     sx: {
-      bgcolor: stringToColor(name),
+      bgcolor: getRandomHexColor(),
     },
-    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    children: `${name.split(" ")[0][0]}`,
   };
 }
 
-export default function BackgroundLetterAvatar({name}:{name:string}) {
-  return (
-      <Avatar {...stringAvatar(name)} />
-
-  );
+export default function BackgroundLetterAvatar({ name }: { name: string }) {
+  return <Avatar {...stringAvatar(name)} />;
 }
